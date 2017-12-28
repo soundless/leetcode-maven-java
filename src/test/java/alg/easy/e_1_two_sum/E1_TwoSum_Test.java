3c3dd5a5b8a57ed8ee972ab38036ffdf6a2ccc99
soundlessthj <soundless.thj@gmail.com>
2017-12-28 12:40:20 -0800

    Rename packages

---
+++ src/test/java/alg/easy/e_1_two_sum/E1_TwoSum_Test.java
@@ -0,0 +1,19 @@
+package alg.easy.e_1_two_sum;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class E1_TwoSum_Test {
+
+    @Test
+    public void test_TwoSum() {
+        int[] data = {9, 7, 2, 11};
+        int[] expected = {2, 3};
+
+        Solution solution = new Solution();
+        int[] actual = solution.twoSum(data, 9);
+
+        assertArrayEquals(expected, actual);
+    }
+}
---
+++ src/test/java/alg/median/m_3_longest_substring_without_repeating_characters/M3_LongestSubstringWithoutRepeatingCharactersTest.java
@@ -0,0 +1,22 @@
+package alg.median.m_3_longest_substring_without_repeating_characters;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class M3_LongestSubstringWithoutRepeatingCharactersTest {
+    @Test
+    public void testSolution() {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        hashMap.put("abcabcbb", 3);
+        hashMap.put("bbbbb", 1);
+        hashMap.put("pwwkew", 3);
+        hashMap.put("abbc", 2);
+        Solution solution = new Solution();
+        for (Map.Entry m:hashMap.entrySet()) {
+            assertEquals(m.getValue(), solution.lengthOfLongestSubstring(m.getKey().toString()));
+        }
+    }
+}
